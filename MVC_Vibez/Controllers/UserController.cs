4d8ea219e67bfde946c4361e87e0dcfdb7a6c3d2
GauteLauwers <Gaute0710@gmail.com>
2024-03-13 09:20:57 +0100

    add project MVC

---
+++ MVC_Vibez/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Vibez.Controllers
+{
+    public class UserController : Controller
+    { 
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
