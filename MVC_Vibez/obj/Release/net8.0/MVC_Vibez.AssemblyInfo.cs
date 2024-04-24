41b8db7f52ab8aeee8307f372a62b8a9ee8dfb40
mathis1205 <96077034+mathis1205@users.noreply.github.com>
2024-04-24 14:16:23 +0200

    temp commit

--- MVC_Vibez/obj/Release/net8.0/MVC_Vibez.AssemblyInfo.cs
+++ MVC_Vibez/obj/Release/net8.0/MVC_Vibez.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC_Vibez")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f878c2a47e2280fca0559377b2da769779c20d99")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC_Vibez")]
