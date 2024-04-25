a3bed37cc4ab4ab73247b8e612e8b5194b22dd6b
mathis1205 <96077034+mathis1205@users.noreply.github.com>
2024-04-25 16:47:33 +0200

    API searchbar

--- MVC_Vibez/Controllers/ProgramController.cs
+++ MVC_Vibez/Controllers/ProgramController.cs
@@ -1,23 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MVC_Vibez.Models;
 
-namespace MVC_Vibez.Controllers
+namespace MVC_Vibez.Controllers;
+
+public class ProgramController : Controller
 {
-    public class ProgramController : Controller
+    public IActionResult Index()
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-        [HttpPost] public async Task<ActionResult> Search(string searchText)
-        {
-	        try
-	        {
-		        var result = await SearchHelper.SearchArtistOrSong(searchText);
-		        if (result == null) return Json(new { success = false });
-                var listArtist = result.artists.items.Select(item => new SpotifyArtist() { ID = item.id, Image = item.images.Any() ? item.images[0].url : "https://user-images.githubusercontent.com/24848110/33519396-7e56363c-d79d-11e7-969b-09782f5ccbab.png", Name = item.name }).ToList(); return Json(new { success = true, artists = listArtist });
-	        }
-	        catch (Exception ex) { return Json(new { success = false, error = ex.Message }); }
-        }
+        return View();
+    }
+
+	[HttpPost]
+	public async Task<ActionResult> Autocomplete(string searchText)
+	{
+		try
+		{
+			var result = await SearchHelper.SearchArtistOrSong(searchText);
+
+			if (result == null)
+			{
+				return Json(new { success = false });
+			}
+
+			var artists = result.artists.items.Select(item => new SpotifyArtist
+			{
+				ID = item.id,
+				Image = item.images.Any() ? item.images[0].url : "https://user-images.githubusercontent.com/24848110/33519396-7e56363c-d79d-11e7-969b-09782f5ccbab.png",
+				Name = item.name
+			}).ToList();
+
+			return Json(new { success = true, artists });
+		}
+		catch (Exception ex)
+		{
+			return Json(new { success = false, error = ex.Message });
+		}
 	}
 }
