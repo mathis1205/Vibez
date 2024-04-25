2de2ed3086c60174416001d83dc9fb0086577b98
Brecht-Vandekerkhove <157580961+Brecht-Vandekerkhove@users.noreply.github.com>
2024-04-25 14:03:31 +0200

    added library View and control + ui Elements

---
+++ MVC_Vibez/Controllers/LibraryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Vibez.Controllers
+{
+    public class LibraryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/Library/Index.cshtml");
+        }
+    }
+}
