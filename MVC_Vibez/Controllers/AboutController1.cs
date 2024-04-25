5db9dd99e0a33946a911483c15cfc264913f7f81
Brecht-Vandekerkhove <157580961+Brecht-Vandekerkhove@users.noreply.github.com>
2024-04-25 10:41:22 +0200

    please work

---
+++ MVC_Vibez/Controllers/AboutController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Vibez.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/About/Index.cshtml");
+        }
+    }
+}
