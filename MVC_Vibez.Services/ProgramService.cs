02509f40e019853257bff734601d1c01a15ff3dd
Hidetsugu666 <nicolas.goemanne@live.nl>
2024-05-08 13:56:18 +0200

    startup add to favorites logic integrated

--- MVC_Vibez.Services/ProgramService.cs
+++ MVC_Vibez.Services/ProgramService.cs
@@ -8,4 +8,10 @@ public class ProgramService
     private readonly VibezDbContext _context;
     public ProgramService(VibezDbContext context) => _context = context;
     public User GetUserByEmail(string email) { return _context.Users.FirstOrDefault(u => u.Email == email); }
+
+    public void UpdateUser(User user)
+    {
+        _context.Users.Update(user);
+        _context.SaveChanges();
+    }
 }
