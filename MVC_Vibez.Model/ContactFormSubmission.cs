7f97c2f733e77ef489fe15c27d77f959592b2d5f
Nicolas-Goemanne <nicolas.goemanne@live.nl>
2024-05-30 19:05:38 +0200

    added a please enter valid email adress to input of contact form

--- MVC_Vibez.Model/ContactFormSubmission.cs
+++ MVC_Vibez.Model/ContactFormSubmission.cs
@@ -4,7 +4,11 @@ namespace MVC_Vibez.Model
 {
     public class ContactFormSubmission
     {
-        [Required] public string Email { get; set; }
-        [Required] public string Message { get; set; }
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required]
+        public string Message { get; set; }
     }
 }
