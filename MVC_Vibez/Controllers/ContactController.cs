bb35087b18b93646ba1ea205891edbcd5546a198
mathis1205 <96077034+mathis1205@users.noreply.github.com>
2024-05-31 09:22:52 +0200

    hotfix contactform

--- MVC_Vibez/Controllers/ContactController.cs
+++ MVC_Vibez/Controllers/ContactController.cs
@@ -2,6 +2,7 @@
 using MVC_Vibez.Model;
 using MVC_Vibez.Models;
 using MVC_Vibez.Services;
+using Org.BouncyCastle.Crypto.Macs;
 
 namespace MVC_Vibez.Controllers;
 
@@ -27,7 +28,7 @@ public class ContactController : Controller
     public async Task<IActionResult> SubmitContactForm(ContactFormSubmission contactForm)
     {
         if (!ModelState.IsValid) return View("Index");
-        await _contactService.Submit(contactForm.Email, contactForm.Message);
+        await _contactService.Submit("Vibezteamhelp@gmail.com", contactForm.Message);
         TempData["SuccessMessage"] = "Thanks for the contacting us, we will answer as fast as possible.";
         return RedirectToAction("Index");
     }
